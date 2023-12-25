45c0d9d4c1913b12d74a984d91fecd5b0f833b88
Nusab Taha <nusabtaha33@gmail.com>
2023-12-26 01:55:44 +0600

    Static export

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -147,12 +147,13 @@ export default Contact;
 
 async function sendMessage({ name, message }) {
   const jsonData = { name, message };
-  const res = await fetch("/api/sendMessage", {
+  const res = await fetch("https://nusab19.pages.dev/api/sendMessage", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify(jsonData),
+    
   });
   const resp = await res.json();
   if (!resp.ok) throw new Error(resp.message);
