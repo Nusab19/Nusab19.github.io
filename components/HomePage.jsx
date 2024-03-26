2765ad2eca87c82303ef58bd5a69b924562ceaa9
Nusab Taha <nusabtaha33@gmail.com>
2024-03-27 02:47:04 +0600

    blur the blobs, add a `<hr/>` under the text

--- components/HomePage.jsx
+++ components/HomePage.jsx
@@ -16,30 +16,33 @@ const blob = (
 const HomePage = () => {
   return (
     <div
-      className={`${merienda.className} relative bg-[#072029] text-gray-100 overflow-hidden`}
+      className={`${merienda.className} relative overflow-hidden bg-[#072029] text-gray-100`}
     >
       <div className="container mx-auto">
         <div className="flex min-h-screen flex-row items-center justify-center">
           <h1 className="magic-text text-4xl font-bold md:text-6xl">
             Hi, I&apos;m Nusab
           </h1>
+
+          <hr className="mt-14 w-[300px] border-t-4 rounded-sm border-[#f3f4f6] md:mt-20 md:w-[500px] absolute" />
+
           <span className="ml-3 mt-3 h-4 w-4 rounded-full bg-[#00bbd4] md:mt-7 md:h-5 md:w-5"></span>
         </div>
       </div>
 
-      <span className="absolute top-[20vh] -translate-x-1/2 text-[#00d49f] come-in">
+      <span className="come-in absolute top-[20vh] -translate-x-1/2 text-[#00d49f] blur-sm">
         {blob}
       </span>
-      <span className="absolute bottom-[23vh] left-[13vw] text-[#dad7cd] come-in">
+      <span className="come-in absolute bottom-[23vh] left-[13vw] text-[#dad7cd] blur-sm">
         {blob}
       </span>
-      <span className="absolute bottom-[10vh] right-[20vw] text-[#f35f5f] come-in">
+      <span className="come-in absolute bottom-[10vh] right-[20vw] text-[#f35f5f] blur-sm">
         {blob}
       </span>
-      <span className="absolute right-[30vw] top-[15vh] text-[#219ebc] come-in">
+      <span className="come-in absolute right-[30vw] top-[15vh] text-[#219ebc] blur-sm">
         {blob}
       </span>
-      <span className="absolute right-0 top-[40vh] translate-x-1/2 text-[#8d99ae] come-in">
+      <span className="come-in absolute right-0 top-[40vh] translate-x-1/2 text-[#8d99ae] blur-sm">
         {blob}
       </span>
     </div>
