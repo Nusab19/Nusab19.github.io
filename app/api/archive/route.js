74dfd8ce71afe72bfc8f610a7d35138885053e33
Nusab Taha <85403795+Nusab19@users.noreply.github.com>
2024-07-02 06:23:15 +0600

    Update route.js

--- app/api/archive/route.js
+++ app/api/archive/route.js
@@ -7,8 +7,8 @@ export async function POST(req) {
   const jsonData = await req.json();
   const ip = req.headers.get("x-real-ip") ?? "127.0.0.1";
   const url = decodeURI((jsonData.url || "").trim());
-
-  return new NextResponse(JSON.stringify({message:" Hello", url, ip}), { status: 200 });
+  console.log(url)
+  return new NextResponse(JSON.stringify({message:" Hello", ip}), { status: 200 });
   
   if(url == ''){
       let resp = {
